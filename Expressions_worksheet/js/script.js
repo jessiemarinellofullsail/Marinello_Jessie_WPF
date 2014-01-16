1d70756ec4e4ae7ae8c18ffc483a24b70e5e661d
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-01-16 16:10:21 -0500

    Dog Years expression

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,3 +4,11 @@
 //Jessie Marinello, Expressions Worksheet
 //January 2014
 
+
+//Dog Years
+var humanAge = 12; //dog's age in human years
+const dogYears = 7; //"dog years" per human year. this remains constant
+//multiply humanAge and constant and assign to dogAge
+var dogAge = humanAge * dogYears;
+//print output age of dog in dog years
+console.log("Sparky is " + humanAge + " human years, which is " + dogAge + " in dog years.");
