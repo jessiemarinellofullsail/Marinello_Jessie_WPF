ab414b0e2801267d4cbc1c2d48666b2c77d57f56
Jessie Marinello <jessiemarinello@fullsail.edu>
2014-01-16 16:40:27 -0500

    Slices of Pie, Parts I and II

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -12,3 +12,18 @@ const dogYears = 7; //"dog years" per human year. this remains constant
 var dogAge = humanAge * dogYears;
 //print output age of dog in dog years
 console.log("Sparky is " + humanAge + " human years, which is " + dogAge + " in dog years.");
+
+//Slice of Pie, Part I
+var numPeople = 48; //this is the total number of people at the party
+var numPizza = 7; //total number of pizzas ordered
+var numSlices = 16; //total number of slices per pizza.
+//multiply numSlices and numPizza then divide product by numPeople.  assign to totalSlices
+var totalSlices = (numSlices * numPizza)/numPeople;
+//print output number of total slices each person ate at the party
+console.log("Each person ate " + totalSlices + " slices of pizza at the party.");
+
+//Slice of Pie, Part II
+//use same variables as Slice of Pie, Part I
+//subtract the product of numSlices and numPeople from totalSlices.  assign to dogSlices
+var remainder = totalSlices % 1;
+console.log("Sparky got" +" "+ remainder +" "+ "slices of pizza")
